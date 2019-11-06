6af83e06a144bfd2de8e01608371b419b1584171
BrianPW <brian.paquelet@gmail.com>
2019-11-06 13:41:10 +0100

    merged test.py into script.py

--- script.py
+++ script.py
@@ -1,3 +1,17 @@
 '''sting file'''
 
 print('Hello world!')
+print('Hello world!')
+
+def circle_circum(r):
+	''' this functioncomputes the circumferance of a cirle '''
+
+	circum = 2*22/7*r 
+	print(circum)
+circle_circum(2)
+
+
+def circle_area(r):
+	area = 22/7*r**2
+	print(area)
+circle_area(3)
